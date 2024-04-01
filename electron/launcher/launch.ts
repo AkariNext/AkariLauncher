1c159824ebe4e1a67785ba28b656045372b74718
yupix <yupi0982@outlook.jp>
2024-04-02 01:32:22 +0900

    feat: プロフィールごとに1つだけ起動できるように

--- electron/launcher/launch.ts
+++ electron/launcher/launch.ts
@@ -7,6 +7,7 @@ import { checkFileExists, recreateDir } from "../utils/dir";
 import nFetch from 'node-fetch'
 import { IAuth, ILauncherOptions } from "node_modules/@akarinext/minecraft-java-core/build/Launch";
 import { updateRepositories } from "../downloader";
+import { minecraftProcesses } from "../utils/state";
 
 (async () => {
     updateRepositories()
@@ -49,7 +50,7 @@ export async function launch() {
             // @ts-ignore
             xuid: minecraft.xuid,
             // clientId:
-        },        
+        },
     }
     
     let opts: ILauncherOptions = {
@@ -87,44 +88,62 @@ export async function launch() {
         }
     }
 
-    await client.Launch(opts)
-    
-    client.on('extract', extract => {
+    // TODO: ここを今後そのプロファイルから自動で取得できるようにするべき
+    const PROFILE_NAME = "Enter a your secure password"
+
+    const currentProcesses = minecraftProcesses.get()
+    const foundProcesses = currentProcesses.find((profile) => profile.profileName == PROFILE_NAME)
+
+    console.log(currentProcesses, foundProcesses)
+
+    if (!foundProcesses) {
+        minecraftProcesses.set([...currentProcesses, {profileName: PROFILE_NAME}])
+        await client.Launch(opts)
+    } else {
+        // ここからrenderの方に何か飛ばすようにする
+    }
+
+    client.on('extract', (extract: any) => {
         console.log(extract);
     });
 
-    client.on('progress', (progress, size, element) => {
+    client.on('progress', (progress: number, size: number, element: any) => {
         console.log(`Downloading ${element} ${Math.round((progress / size) * 100)}%`);
     });
 
-    client.on('check', (progress, size, element) => {
+    client.on('check', (progress: number, size: number, element: any) => {
         console.log(`Checking ${element} ${Math.round((progress / size) * 100)}%`);
     });
 
-    client.on('estimated', (time) => {
+    client.on('estimated', (time: number) => {
         let hours = Math.floor(time / 3600);
         let minutes = Math.floor((time - hours * 3600) / 60);
         let seconds = Math.floor(time - hours * 3600 - minutes * 60);
         console.log(`${hours}h ${minutes}m ${seconds}s`);
+
     })
 
-    client.on('speed', (speed) => {
+    client.on('speed', (speed: number) => {
         console.log(`${(speed / 1067008).toFixed(2)} Mb/s`)
     })
 
-    client.on('patch', patch => {
+    client.on('patch', (patch: any) => {
         console.log(patch);
     });
 
-    client.on('data', (e) => {
+    client.on('data', (e: any) => {
         console.log(e);
     })
 
-    client.on('close', code => {
+    client.on('close', (code: any) => {
         console.log(code);
+        // 現在のプロファイルを一覧から削除する
+        const currentProcesses = minecraftProcesses.get().filter((profile) => profile.profileName !== PROFILE_NAME)
+
+        minecraftProcesses.set(currentProcesses)
     });
 
-    client.on('error', err => {
+    client.on('error', (err: any) => {
         console.log(err);
     });
     // client.on('progress', (e) => console.log(e))
