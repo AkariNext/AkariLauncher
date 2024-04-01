0db65429e10b0b8250bd9beb0b9373ebf7e11010
yupix <yupi0982@outlook.jp>
2024-04-02 03:13:44 +0900

    refactor: サーバーセレクターをより汎用的に使えるように改良

--- src/components/ServerSelector/Server.vue
+++ src/components/ServerSelector/Server.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
+
 const props = defineProps({
-    serverName: { type: String, required: true },
-    mcVersion: { type: String, required: true },
-    packVersion: String,
-    packDescription: String,
-    packIconUrl: String,
+    server: {
+        type: Object as () => {
+            serverName: string,
+            mcVersion: string,
+            packVersion?: string,
+            packDescription: string,
+            packIconUrl?: string,
+        },
+        required: true
+    },
+    opacity: { type: Number, default: 0.6 }
 })
+
+const emit = defineEmits<{
+    (e: 'select', selectedServerName: string): void,
+}>()
+
 </script>
 
 <template>
-    <div class="max-w-sm flex text-white w-full px-1 py-1 gap-4 items-center"
-        style="background: rgba(131, 131, 131, 0.25);">
+    <div class="max-w-sm flex text-white w-full px-1 py-1 gap-4 items-center hover:bg-red-600" @click="emit('select', server.serverName)"
+        style="background: rgba(131, 131, 131, 0.25); opacity: 0.6;" v-bind:style="{ opacity: props.opacity }">
         <img src="/vite.svg" class="border flex-shrink-0 w-14 h-14" />
         <div>
-            <h4 class="text-lg">{{ props.serverName }}</h4>
-            <p class="text-[10px]">{{ props.packDescription }}</p>
+            <h4 class="text-lg">{{ props.server.serverName }}</h4>
+            <p class="text-[10px]">{{ props.server.packDescription }}</p>
             <div id="server-tags" class="flex gap-2">
-                <p class="text-[10px] bg-emerald-700 rounded-sm w-fit px-1">{{ props.mcVersion }}</p>
-                <p class="text-[10px]">{{ props.packVersion }}</p>
+                <p class="text-[10px] bg-emerald-700 rounded-sm w-fit px-1">{{ props.server.mcVersion }}</p>
+                <p class="text-[10px]">{{ props.server.packVersion }}</p>
             </div>
         </div>
     </div>
--- src/components/ServerSelector/ServerSelectModal.vue
+++ src/components/ServerSelector/ServerSelectModal.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { VueFinalModal } from 'vue-final-modal'
 import Server from './Server.vue';
+import { ref } from 'vue';
 
 defineProps<{
     title?: string
@@ -10,22 +11,47 @@ const emit = defineEmits<{
     (e: 'confirm'): void,
     (e: 'close'): void
 }>()
+
+const servers = ref<{
+    serverName: string
+    mcVersion: string
+    packVersion: string,
+    packDescription: string,
+    packIconUrl?: string,
+}[]>([
+    {
+        serverName: "test",
+        mcVersion: "1.12.1",
+        packVersion: "1.0.0",
+        packDescription: "これはテスト用です",
+    },
+    {
+        serverName: "test1",
+        mcVersion: "1.12.1",
+        packVersion: "1.0.0",
+        packDescription: "これはテスト用です",
+    },
+    {
+        serverName: "test2",
+        mcVersion: "1.12.1",
+        packVersion: "1.0.0",
+        packDescription: "これはテスト用です",
+    }
+
+])
+
+const selectedServer = ref<string>("test2")
 </script>
 
 <template>
     <VueFinalModal overlay-transition="vfm-fade" content-transition="vfm-fade" class="flex justify-center items-center"
-        content-class="w-full">
+        content-class="w-full" overlay-style="filter: blur(10px)">
         <div class="w-full flex flex-col items-center">
             <h3 class="text-white text-xl font-bold mb-4">Avaliable Servers</h3>
             <div class="flex flex-col items-center gap-2 w-full overflow-y-scroll max-h-72 max-w-md">
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
-                <Server server-name="aaa" pack-description="これはテスト用です" mc-version="1.12.1" pack-version="1.0.0" />
+                <Server v-for="server in servers" :key="server.serverName" :server="server"
+                    :opacity="server.serverName === selectedServer ? 1 : 0.6"
+                    @select="(serverName) => selectedServer = serverName" />
             </div>
             <div class="flex flex-col">
                 <button class="mt-4 max-w-sm text-white text-sm px-1 border rounded-sm" @click="emit('confirm')">
@@ -43,10 +69,10 @@ const emit = defineEmits<{
 <style scoped>
 ::-webkit-scrollbar {
     background: rgba(0, 0, 0, 0.632);
-    width:5px;
+    width: 5px;
 }
 
 ::-webkit-scrollbar-thumb {
-  background-color: rgb(204, 204, 204);
+    background-color: rgb(204, 204, 204);
 }
 </style>
