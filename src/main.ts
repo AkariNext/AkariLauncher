dbe372dbe63ff8b0f09fb6624778867920a7a145
yupix <yupi0982@outlook.jp>
2024-04-02 01:28:27 +0900

    feat: view final router を読み込むように & routerを読むこむように
    Co-authored-by: sousuke0422 <sousuke0422@outlook.jp>

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import VueAnimXyz from '@animxyz/vue3'
 import '@animxyz/core'  // animxyz core styles
-const pinia = createPinia()
+import { createVfm } from 'vue-final-modal'
 
+import './style.css'
+import 'vue-final-modal/style.css'
+import { router } from './routes'
+
+
+const pinia = createPinia()
+const vfm = createVfm()
 const app = createApp(App)
+app.use(vfm)
+app.use(router)
 app.use(pinia)
 app.use(VueAnimXyz)
 
