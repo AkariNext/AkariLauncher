322de2529481fed71205cf237ced705cef75fe68
yupix <yupi0982@outlook.jp>
2024-04-02 03:12:46 +0900

    feat: vue routerを使うように
    Co-authored-by: sousuke0422 <sousuke0422@outlook.jp>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
-import App from '../App.vue'
+import Home from './Home.vue'
 import Signin from '../components/Signin.vue'
 import GameLog from '../components/GameLog.vue'
 
 const routes: RouteRecordRaw[] = [
-    { path: '/', component: App },
-    { path: '/signin', component: Signin},
+    { path: '/', component: Home },
+    { path: '/signin', component: Signin },
     { path: '/gamelog', component: GameLog }
 ]
 
