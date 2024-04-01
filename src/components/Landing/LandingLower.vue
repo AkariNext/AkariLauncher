8b1ec7c5e4973ce1b644b4bc536878550296c2e4
yupix <yupi0982@outlook.jp>
2024-04-02 01:31:37 +0900

    feat: サーバーセレクターを使用できるように

--- src/components/Landing/LandingLower.vue
+++ src/components/Landing/LandingLower.vue
@@ -1,22 +1,57 @@
 <script setup lang="ts">
+import { ModalsContainer, useModal } from 'vue-final-modal'
+import ServerSelectModal from '../ServerSelector/ServerSelectModal.vue'
+
 async function func() {
     await window.electron.ipcRenderer.invoke('gameLaunch')
 }
+
+
+const {open, close} = useModal({
+    component: ServerSelectModal,
+    
+    attrs: {
+        title: 'Hello',
+        onConfirm() {
+            close()
+        },
+        onClose() {
+            close()
+        }
+    },
+    slots: {
+        default: ''
+    }
+})
 </script>
 
 <template>
     <div :class="$style.lower">
         <div :class="$style.lower_left">
             <div>
-                <div :class="$style.lower_left_content">
-                    <div :class="$style.server_status_wrapper">
-                        <span :class="$style.bot_label">SERVER</span>
+                <div class="relative flex gap-4 items-center top-[25px] left-[50px]">
+                    <div class="flex items-center">
+                        <span class="text-white drop-shadow-lg shadow-black font-bold text-xs">SERVER</span>
                         <span :class="$style.player_count">OFFLINE</span>
                     </div>
-                    <div :class="$style.bot_divider"></div>
-                    <div :class="$style.mojangStatusWrapper">
-                        <span :class="$style.bot_label">MOJANG STATUS</span>
-                        <span :class="$style.mojang_status_icon" style="color: rgb(165, 195, 37);">・</span>
+                    <div class="bg-gray-500 w-[2px] h-6"></div>
+                    <div class="relative group flex items-center cursor-pointer">
+                        <div>
+                            <span class="text-white drop-shadow-lg shadow-black font-bold text-xs">MOJANG STATUS</span>
+                            <div class="absolute p-2 w-full text-white -top-[115px] invisible group-hover:visible rounded-sm"
+                                style="background: rgba(0, 0, 0, 0.75);">
+                                <h1 class="text-center">Services</h1>
+                                <div class="flex items-center gap-1 mt-2">
+                                    <div class="h-2 w-2 bg-lime-300 rounded-full flex-shrink-0"></div>
+                                    <p class="text-[10px]">Multiplayer Session Service</p>
+                                </div>
+                                <div class="flex items-center gap-1 mt-1">
+                                    <div class="h-2 w-2 bg-lime-300 rounded-full flex-shrink-0"></div>
+                                    <p class="text-[10px]">Microsoft Oauth Session</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="h-3 w-3 bg-lime-300 rounded-full ml-2"></div>
                     </div>
                 </div>
             </div>
@@ -27,9 +62,11 @@ async function func() {
         <div :class="$style.lower_right">
             <div>
                 <div :class="$style.launch_content">
-                    <button :class="$style.launch_button" class="text-white" @click="async () => await func()">PLAY</button>
+                    <button :class="$style.launch_button" class="text-white"
+                        @click="async () => await func()">PLAY</button>
                     <div :class="$style.bot_divider"></div>
-                    <button :class="[$style.server_selection_button, $style.bot_label]" class="text-white">・Demo (Minecraft
+                    <button :class="[$style.server_selection_button, $style.bot_label]" class="text-white" @click="() => open()">・Demo
+                        (Minecraft
                         1.19.4)</button>
                 </div>
             </div>
@@ -51,6 +88,7 @@ async function func() {
     cursor: pointer;
 }
 
+
 .server_selection_button {
     background: none;
     border: none;
@@ -63,7 +101,6 @@ async function func() {
 
 .server_selection_button:hover {
     text-shadow: 0px 0px 20px #fff, 0px 0px 20px #fff, 0px 0px 20px #fff;
-
 }
 
 .launch_button {
@@ -94,7 +131,6 @@ async function func() {
     width: 2px;
     background: rgba(107, 105, 105, 0.7);
     margin-left: 20px;
-    margin-right: 20px;
 }
 
 .player_count {
@@ -164,5 +200,3 @@ async function func() {
 
 }
 </style>
-
-
