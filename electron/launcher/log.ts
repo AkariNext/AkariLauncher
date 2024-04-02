4d1c93dba379549601e0db11eced294b55552e63
yupix <yupi0982@outlook.jp>
2024-04-02 21:51:44 +0900

    chore: ログウィンドウのツールバーを非表示に
    Co-authored-by: sousuke0422 <sousuke0422@outlook.jp>

--- electron/launcher/log.ts
+++ electron/launcher/log.ts
@@ -5,8 +5,8 @@ import path from 'node:path'
 // todo: GameLog.vueをいい感じに出す場所
 export function createGameLogWindow() {
     const logWindow = new BrowserWindow({
-        darkTheme: true,
         title: 'Game Log',
+        backgroundMaterial: 'mica',
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: false,
@@ -14,6 +14,8 @@ export function createGameLogWindow() {
             contextIsolation: true
           },
     })
+
+    logWindow.setMenuBarVisibility(false)
 
     if (VITE_DEV_SERVER_URL) {
         logWindow.loadURL(VITE_DEV_SERVER_URL + '#/gamelog')
