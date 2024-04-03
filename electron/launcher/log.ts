d3f1e16f13f9b1b3a4cec7083e8ffe1471831175
yupix <yupi0982@outlook.jp>
2024-04-03 18:05:30 +0900

    chore: logwindowにiconを設定
    Co-authored-by: sousuke0422 <sousuke0422@outlook.jp>

--- electron/launcher/log.ts
+++ electron/launcher/log.ts
@@ -4,6 +4,7 @@ import path from 'node:path'
 
 export function createGameLogWindow(opts: { profileName: string }) {
     const logWindow = new BrowserWindow({
+        icon: path.join(process.env.PUBLIC, 'vite.svg'),
         title: 'Game Log',
         backgroundMaterial: 'mica',
         webPreferences: {
@@ -17,6 +18,7 @@ export function createGameLogWindow(opts: { profileName: string }) {
     logWindow.setMenuBarVisibility(false)
 
     if (VITE_DEV_SERVER_URL) {
+        // setTitleがindex.htmlのtitleで上書きされる対策 https://github.com/electron/electron/issues/2543#issuecomment-876309955
         logWindow.loadURL(VITE_DEV_SERVER_URL + '#/gamelog').then(() => logWindow.setTitle(`AkariLauncher - ${opts.profileName} Log`))
     } else {
         logWindow.loadFile(path.join(process.env.DIST, 'index.html'), { hash: 'gamelog' }).then(() => logWindow.setTitle(`AkariLauncher - ${opts.profileName} Log`))
