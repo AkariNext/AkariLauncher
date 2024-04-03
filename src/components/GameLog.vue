18fbb88ece772575da765d06e2c8c8f837066ed2
yupix <yupi0982@outlook.jp>
2024-04-03 18:04:56 +0900

    feat: terminalでコピーを可能に
    Co-authored-by: sousuke0422 <sousuke0422@outlook.jp>

--- src/components/GameLog.vue
+++ src/components/GameLog.vue
@@ -13,6 +13,11 @@ interface ElectronWindow extends Window {
 }
 declare const window: ElectronWindow;
 
+const lightRedTextColor = '\x1b[91m';
+const purpleTextColor = '\x1b[38;2;255;200;255m';
+
+const resetTextColor = '\x1b[0m';
+
 onMounted(() => {
     const term = new Terminal({
         rows: 30,
@@ -34,10 +39,27 @@ onMounted(() => {
 
     fitAddon.fit();
 
-    
+    term.attachCustomKeyEventHandler((event) => {
+        if (event.ctrlKey && event.code == 'KeyC' || event.ctrlKey && event.shiftKey && event.code == 'KeyC') {
+            const text = window.getSelection()?.toString();
+            if (text) {
+                navigator.clipboard.writeText(text);
+            }
+        }
+
+        return false;
+    })
 
     window.electron.ipcRenderer.on('logWindow.data', (_, data) => {
-        term.write(data);
+        term.writeln(data);
+    });
+
+    window.electron.ipcRenderer.on('logWindow.close', (_, data) => {
+        term.write(purpleTextColor + data + resetTextColor);
+    });
+
+    window.electron.ipcRenderer.on('logWindow.error', (_, data) => {
+        term.write(lightRedTextColor + data + resetTextColor);
     });
 });
 </script>
